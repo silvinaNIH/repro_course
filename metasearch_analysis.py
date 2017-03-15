dff657e2528274ab30c7f7bf67a3e792797e5654
Silvina Horovitz <Silvina.Horovitz@nih.gov>
2017-03-15 11:28:08 -0400

    move metasearch into untracked directory

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,3 +1,6 @@
 # coding: utf-8
 #comment next line so it doesnt load again
 #get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
+ 
+Path('data_not_in_repo').mkdir()
+get_ipython().magic('mv metasearch data_not_in_repo/')
