6640e19ef3a1865d5ae71a2937260e47657ccb26
Silvina Horovitz <Silvina.Horovitz@nih.gov>
2017-03-15 12:04:44 -0400

    test data is downloaded

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,6 +1,12 @@
 # coding: utf-8
-#comment next line so it doesnt load again
+#Download our data from openneu.ro
 #get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
- 
-Path('data_not_in_repo').mkdir()
-get_ipython().magic('mv metasearch data_not_in_repo/')
+#Path('data_not_in_repo').mkdir()
+from pathlib import Path
+metasearch_dir = Path('data_not_in_repo/metasearch/')
+data_exists = metasearch_dir.exists()
+in_repro_course = Path.cwd().name == 'repro_course'
+if data_exists and in_repro_course:
+    print('data has been downloaded')
+else:
+    print ('data has not been found')
